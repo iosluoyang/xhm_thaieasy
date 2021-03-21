496d241287d30b1f58df7db94833c616de75c457
HYOneMore <ioslhy@163.com>
2021-03-22 00:28:12 +0800

    update

--- src/pages/wishproduct/productlist.js
+++ src/pages/wishproduct/productlist.js
@@ -1,7 +1,10 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
+import store from '@/store';
 import { useHistory, useParams } from 'react-router-dom';
 import { Box, AppBar, Tabs, Tab, List, ListItem, ListItemAvatar, ListItemText, Fab, Container } from '@material-ui/core';
+import { VariableSizeList as ReactWindowList } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
 import { makeStyles } from '@material-ui/core/styles';
 import utils from '@/utils';
 import { Add } from '@material-ui/icons';
@@ -12,27 +15,7 @@ import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/effect-cube/effect-cube.scss';
 import { getproductlistapi } from '@/api/productapi';
 
-
-// 自定义hook实现
-const useSyncCallback = callback => {
-    const [proxyState, setProxyState] = useState({ current: false })
-
-    const Func = useCallback(() => {
-        setProxyState({ current: true })
-    }, [proxyState])
-
-    useEffect(() => {
-        if (proxyState.current === true) setProxyState({ current: false })
-    }, [proxyState])
-
-    useEffect(() => {
-        proxyState.current && callback()
-    })
-
-    return Func
-}
-
-function WishProduct(props) {
+function ProductList(props) {
 
     const history = useHistory()
     const ref = useRef({ swiper: null })
@@ -83,10 +66,12 @@ function WishProduct(props) {
 
     const classes = useStyles()
 
-    let searchText = ''
+    let searchText = '' // 搜索文本
+    // 开始搜索
     const startToSearch = (searchContent) => {
-        console.log(`开始搜索,搜索文本为${searchContent}`)
         searchText = searchContent
+        console.log(`搜索前的索引为${initTabIndex}`)
+        // 此处搜索的时候取值索引会为初始值  ...
         // 加载商品列表数据
         loadData(true)
     }
@@ -96,32 +81,32 @@ function WishProduct(props) {
         pageSize: 10,
         date: ''
     }
-
-    let owntabs = [
+    // nav的数组  记录当前页码 是否加载等信息
+    let navTabs = [
         {
             title: `${'全部'}`,
             type: -1,
-            dataList: [],
             loaded: false,
             pageInfo: initPageInfo
         },
         {
             title: `${'任务中'}`,
             type: 0,
-            dataList: [],
             loaded: false,
             pageInfo: initPageInfo
         },
         {
             title: `${'未添加任务'}`,
             type: 1,
-            dataList: [],
             loaded: false,
             pageInfo: initPageInfo
         }
     ]
 
-    const [tabs, setTabs] = useState(owntabs)
+    // 列表的数据源数组
+    let initDataTabs = navTabs.map(() => ([]))
+    const [dataTabs, setDataTabs] = useState(initDataTabs)
+
     let initTabIndex = 0
     const [currentTabIndex, setCurrentTabIndex] = useState(initTabIndex)
     const handleChangeTab = (event, index) => {
@@ -130,13 +115,22 @@ function WishProduct(props) {
         if (ref.current && ref.current.swiper) ref.current.swiper.slideTo(index, 300)
         loadData()
     }
+    const handleChangeSwiper = (swiper) => {
+        let index = swiper.realIndex
+        setCurrentTabIndex(index)
+        initTabIndex = index
+        loadData()
+    }
+
+    let listType = 'normallist' // 当前列表项的渲染类型  normallist 为正常渲染  reactwindowlist 为react-window虚拟列表渲染模式
 
     // 加载当前数据
     const loadData = (refresh) => {
 
         // 获取商品列表数据
+
         // 获取当前tab对象
-        let currentTab = { ...tabs[initTabIndex] } // 深拷贝对象
+        let currentTab = navTabs[initTabIndex]
         let jobFlag = currentTab.type
         let loaded = currentTab.loaded
         let pageInfo = currentTab.pageInfo
@@ -149,7 +143,7 @@ function WishProduct(props) {
 
         // 如果没有加载过则进行加载
         if (!loaded) {
-
+            console.log(`此时加载数据前的index为:${initTabIndex}`)
             let data = {
                 ...{ title: searchText },
                 ...{ account: '' }, // 某个客服的特定商品
@@ -165,21 +159,26 @@ function WishProduct(props) {
                 let list = response.data.list || []
                 let date = response.data.date
 
-                setTabs((oldTabs) => {
-                    let tempTabs = [...oldTabs]
-                    let tempCurTab = tempTabs[initTabIndex]
-                    tempCurTab.dataList = pageInfo.pageNum === 1 ? list : tempCurTab.dataList.concat(list)
-                    tempCurTab.loaded = true
-                    tempCurTab.pageInfo = {
-                        pageNum: pageInfo.pageNum + 1,
-                        pageSize: pageInfo.pageSize,
-                        date: pageInfo.pageNum === 1 ? date : pageInfo.date
-                    }
-                    return tempTabs
+                // 变更state数据
+                setDataTabs((oldDataTabs) => {
+                    let tempDataTabs = [...oldDataTabs]
+                    let tempDataList = tempDataTabs[initTabIndex]
+                    tempDataList = data.pageNum === 1 ? list : tempDataList.concat(list)
+                    tempDataTabs.splice(initTabIndex, 1, tempDataList)
+                    return tempDataTabs
                 })
 
-                console.log(`变更后的tabs数据为`)
-                console.log(tabs)
+                // console.log(`变更后的dataTabs数据为(未同步更新)`)
+                // console.log(dataTabs)
+
+                // 变更pageInfo和loaded数据
+                currentTab.loaded = true
+                let newDate = data.pageNum === 1 ? date : pageInfo.date
+                let newPageNum = pageInfo.pageNum + 1
+                pageInfo = { ...pageInfo, ...{ date: newDate }, ...{ pageNum: newPageNum } }
+                currentTab.pageInfo = pageInfo
+                console.log(`变更后的navTabs数据为(同步更新)`)
+                console.log(navTabs)
 
             }).catch(error => {
                 utils.showToast(error.msg, 'error')
@@ -196,10 +195,42 @@ function WishProduct(props) {
 
     }, [])
 
-    // const [swiper, setSwiper] = useState(null)
-
     const toAddPro = () => {
         history.push('/wishproduct/handleproduct/add')
+    }
+
+    // 列表渲染方式为reactwindowlist时的子元素
+    function ReactWindowEachItem(props) {
+
+        // index：列表项的下标；style：列表项的样式（此参数必须传入列表项的组件中，否则会出现滚动到下方出现空白的情况）
+        const { index, style, data } = props
+        let eachitem = data[index]
+
+        let imgUrl = store.getState().appConfig.imgUrl
+
+        return (
+            <ListItem className='eachpro' style={style} divider button key={eachitem.pid}>
+
+                {/* 商品图片 */}
+                {
+                    eachitem.imgs && eachitem.imgs.split(',').length > 0 && imgUrl &&
+                    <ListItemAvatar>
+                        <img className='img' alt={eachitem.title} src={imgUrl + eachitem.imgs.split(',')[0]}></img>
+                    </ListItemAvatar>
+                }
+
+                {/* 商品标题和备注 */}
+                <ListItemText primary={eachitem.title} secondary={eachitem.remark}>
+
+                </ListItemText>
+
+                {/* 操作按钮 */}
+                {
+
+                }
+
+            </ListItem>
+        )
     }
 
     return (
@@ -214,7 +245,7 @@ function WishProduct(props) {
                 <Tabs value={currentTabIndex} onChange={handleChangeTab} indicatorColor='primary' variant='fullWidth'>
 
                     {
-                        owntabs.map((eachitem, index) => {
+                        navTabs.map((eachitem, index) => {
                             return (
                                 <Tab label={eachitem.title} key={index}></Tab>
                             )
@@ -233,44 +264,68 @@ function WishProduct(props) {
                 spaceBetween={0}
                 slidesPerView={1}
                 onInit={(swiper) => { ref.current.swiper = swiper }}
-                onSlideChange={(swiper) => { setCurrentTabIndex(swiper.realIndex) }}
+                onSlideChange={handleChangeSwiper}
             >
 
                 {
-                    tabs.map((eachitem, index) => {
+                    dataTabs.map((dataList, index) => {
                         return (
-                            <SwiperSlide key={eachitem.type} className='eachslide'>
+                            <SwiperSlide key={index} className='eachslide'>
                                 {/* <Box textAlign='center'>{JSON.stringify(eachitem)}</Box> */}
-                                <List className={classes.proList}>
-                                    {
-                                        eachitem.dataList && eachitem.dataList.length > 0 &&
-                                        eachitem.dataList.map((eachitem) => {
-                                            return (
-                                                <ListItem className='eachpro' divider button key={eachitem.pid}>
+                                {
+                                    listType === 'normallist' &&
+                                    <List className={classes.proList}>
+                                        {
+                                            dataList && dataList.length > 0 &&
+                                            dataList.map((eachitem) => {
+                                                return (
+                                                    <ListItem className='eachpro' divider button key={eachitem.pid}>
 
-                                                    {/* 商品图片 */}
-                                                    {
-                                                        eachitem.imgs && eachitem.imgs.split(',').length > 0 &&
-                                                        <ListItemAvatar>
-                                                            <img className='img' alt={eachitem.title} src={props.appConfig.imgUrl + eachitem.imgs.split(',')[0]}></img>
-                                                        </ListItemAvatar>
-                                                    }
+                                                        {/* 商品图片 */}
+                                                        {
+                                                            eachitem.imgs && eachitem.imgs.split(',').length > 0 &&
+                                                            <ListItemAvatar>
+                                                                <img className='img' alt={eachitem.title} src={props.appConfig.imgUrl + eachitem.imgs.split(',')[0]}></img>
+                                                            </ListItemAvatar>
+                                                        }
 
-                                                    {/* 商品标题和备注 */}
-                                                    <ListItemText primary={eachitem.title} secondary={eachitem.remark}>
+                                                        {/* 商品标题和备注 */}
+                                                        <ListItemText primary={eachitem.title} secondary={eachitem.remark}>
 
-                                                    </ListItemText>
+                                                        </ListItemText>
 
-                                                    {/* 操作按钮 */}
-                                                    {
+                                                        {/* 操作按钮 */}
+                                                        {
 
-                                                    }
+                                                        }
 
-                                                </ListItem>
-                                            )
-                                        })
-                                    }
-                                </List>
+                                                    </ListItem>
+                                                )
+                                            })
+                                        }
+                                    </List>
+                                }
+                                {
+                                    listType === 'reactwindowlist' &&
+                                    <AutoSizer>
+                                        {({ height, width }) => (
+                                            <ReactWindowList
+                                                className={classes.proList}
+                                                width={width}
+                                                height={height}
+                                                itemCount={dataList.length}
+                                                itemData={dataList}
+                                                estimatedItemSize={80}
+                                                itemSize={(index) => (100)}
+                                                itemKey={(index, data) => (data[index].pid)}
+                                            >
+                                                {
+                                                    ReactWindowEachItem
+                                                }
+                                            </ReactWindowList>
+                                        )}
+                                    </AutoSizer>
+                                }
                             </SwiperSlide>
                         )
                     })
@@ -291,4 +346,4 @@ const mapStateToProps = ((state) => {
     return state
 })
 
-export default connect(mapStateToProps, null)(WishProduct)
+export default connect(mapStateToProps, null)(ProductList)
